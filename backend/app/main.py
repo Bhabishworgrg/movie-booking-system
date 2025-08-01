83e24a27b4a86062f2fa26ccbd11301b8d240975
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-08-02 00:04:38 +0545

    Have cleaner router imports

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI, APIRouter 
 from fastapi.responses import RedirectResponse
 
-from app.routers import movie, showtime, booking, seat
+from app.routers import *
 
 app = FastAPI()
 
 router = APIRouter()
-router.include_router(movie.router)
-router.include_router(showtime.router)
-router.include_router(booking.router)
-router.include_router(seat.router)
+router.include_router(movie)
+router.include_router(showtime)
+router.include_router(booking)
+router.include_router(seat)
 
 app.include_router(router)
 
---
+++ backend/app/routers/__init__.py
@@ -0,0 +1,4 @@
+from .movie import router as movie
+from .showtime import router as showtime
+from .booking import router as booking
+from .seat import router as seat
