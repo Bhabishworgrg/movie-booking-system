602af910d3705aaf2550c9783d9209f1ac2d45d2
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-08-01 21:29:35 +0545

    Exclude data if there is none

--- backend/schemas/common.py
+++ backend/schemas/common.py
@@ -1,9 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import Generic, TypeVar
 
 T = TypeVar('T')
 
 class ResponseModel(BaseModel, Generic[T]):
-    data: T
+    data: T | None
     code: int
     message: str
+
+    model_config = ConfigDict(exclude_none=True)
