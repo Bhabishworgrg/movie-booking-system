434ac188ed07d56ffd27b4e35247df2e77e9afda
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-08-04 00:00:13 +0545

    Add jwt creation and decoding methods

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,9 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from datetime import timedelta
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = 'Movie Booking System'
     API_PREFIX: str = '/api/v1'
+    ALGORITHM: str = 'HS256'
+    EXPIRES_DELTA: timedelta = timedelta(hours=1)
     DATABASE_URL: str
+    SECRET_KEY: str
 
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,9 +1,33 @@
 from passlib.context import CryptContext
+from datetime import datetime
+from jwt import encode, decode
 
-password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from app.core.config import settings
+
+password_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 def hash_password(password: str) -> str:
     return password_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return password_context.verify(plain_password, hashed_password)
+
+def create_jwt_token(data: dict) -> str:
+    expire = datetime.utcnow() + settings.EXPIRES_DELTA
+    to_encode = {**data, 'exp': expire}
+     
+    return encode(
+        payload=to_encode,
+        key=settings.SECRET_KEY, 
+        algorithm=settings.ALGORITHM
+    )
+
+def decode_jwt_token(token: str) -> dict | None:
+    try:
+        return decode(
+            jwt=token, 
+            key=settings.SECRET_KEY, 
+            algorithms=[settings.ALGORITHM]
+        )
+    except:
+        return None
